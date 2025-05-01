c7899800cfab2da263e4640f1ea5fb173372cee4
Mayank Shekhar <mayankshekhar0303gmail.com>
2025-05-01 12:18:52 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Weather_App_React/', 
   plugins: [react()],
 })
+
+
